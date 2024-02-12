bbe6e98b703fb6cc5c32fd199611a197d446ec3d
mmrdr <kialisaev@edu.hse.ru>
2024-02-12 22:59:28 +0300

    Дал название папке с консольным приложением

---
+++ GameExe/Program.cs
@@ -0,0 +1,14 @@
+﻿//C:\Users\murd3rRRR\source\repos\KDZ2_mod3_Isaev_Kirill\Smth\bin\Debug\net6.0\18V.JSON
+using ClassLib;
+using System.Text.Json;
+
+namespace StartGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
